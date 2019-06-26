699c35123b20a4ca1731268d6c79a29c31fe51f4
Paulo Henrique <paulohenrique0953@gmail.com>
2019-06-25 21:35:43 -0300

    Script de criação de tabelas de um PETShop

---
+++ petShop_DATABASE.sql
@@ -0,0 +1,38 @@
+CREATE TABLE cliente(
+	ID INT AUTO_INCREMENT,
+	nome VARCHAR(100),
+	endereco VARCHAR(100),
+	telefone VARCHAR(20),
+	PRIMARY KEY(ID)
+);
+
+CREATE TABLE funcionario(
+	ID INT AUTO_INCREMENT,
+	nome VARCHAR(100),
+	PRIMARY KEY(ID)
+);
+
+CREATE TABLE animal(
+	ID INT AUTO_INCREMENT,
+	nome VARCHAR(100),
+	especie VARCHAR(100),
+	raca VARCHAR(20),
+	dono INT,
+	PRIMARY KEY(ID),
+	FOREIGN KEY (dono) REFERENCES cliente(ID)
+);
+
+CREATE TABLE servico(
+	ID INT AUTO_INCREMENT,
+	data DATE,
+	tipo_servico VARCHAR(100),
+	status BOOL,
+	codCliente INT,
+	codAnimal INT,
+	codFuncionario INT, 
+	valor REAL,
+	PRIMARY KEY (ID),
+	FOREIGN KEY(codCliente) REFERENCES cliente(ID),
+	FOREIGN KEY(codAnimal) REFERENCES animal(ID),
+	FOREIGN KEY(codFuncionario) REFERENCES funcionario(ID)
+);
